fca4ca0fcd0b5ecbc55e9901815889abd41f769b
julioc-p <14-10820@usb.ve>
2025-06-07 20:35:25 +0200

    change split name
    Signed-off-by: julioc-p <14-10820@usb.ve>

--- validation/test_set/v2/generation/generate_sparql_queries_base_model_mistral_de_4bit.py
+++ validation/test_set/v2/generation/generate_sparql_queries_base_model_mistral_de_4bit.py
@@ -56,7 +56,7 @@ try:
     dataset = load_dataset(
         "json",
         data_files="/netscratch/jperez/validation_dataset_en_final.json",
-        split="train",
+        split="test",
     )
     print(f"Dataset loaded successfully. Number of examples: {len(dataset)}")
 except Exception as e:
@@ -246,9 +246,7 @@ else:
                     input_ids=inputs["input_ids"],
                     attention_mask=inputs["attention_mask"],
                     max_new_tokens=max_new_tokens_generate,
-                    do_sample=True,
-                    temperature=0.7,
-                    top_p=0.9,
+                    do_sample=False,
                     pad_token_id=tokenizer.pad_token_id,
                 )
         except Exception as e:
